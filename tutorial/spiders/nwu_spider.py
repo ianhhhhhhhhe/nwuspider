f93a3bd9a02c6c1dd4cdf053b0b105f3dbeb0c41
IanPeverell <517382432@qq.com>
2016-10-24 16:39:41 +0800

    Remove useless files, add logging function to spiders

--- tutorial/spiders/nwu_spider.py
+++ tutorial/spiders/nwu_spider.py
@@ -2,6 +2,22 @@
 import scrapy
 from tutorial.items import NWUItem
 
+import glob
+import logging
+import logging.handlers
+
+LOG_FILENAME = 'NWU.out'
+
+my_logger = logging.getLogger('Mylogger')
+my_logger.setLevel (logging.DEBUG)
+
+handler = logging.handlers.RotatingFileHandler(LOG_FILENAME,
+                                               maxBytes = 1048576,
+                                               backupCount = 24,
+                                               )
+my_logger.addHandler(handler)
+
+
 class NwuSpiderSpider(scrapy.Spider):
     name = "nwu"
     allowed_domains = ["nwu.edu.cn"]
@@ -14,4 +30,5 @@ class NwuSpiderSpider(scrapy.Spider):
             item = NWUItem()
             item['title'] = sel.xpath('a/text()').extract()
             item['link'] = sel.xpath('a/@href').extract()
+            my_logger(item)
             yield item
--- tutorial/spiders/nwulab_spider.py
+++ tutorial/spiders/nwulab_spider.py
@@ -2,6 +2,22 @@
 import scrapy
 from tutorial.items import NWUItem
 
+import glob
+import logging
+import logging.handlers
+
+LOG_FILENAME = 'NWULab.out'
+
+my_logger = logging.getLogger('Mylogger')
+my_logger.setLevel (logging.DEBUG)
+
+handler = logging.handlers.RotatingFileHandler(LOG_FILENAME,
+                                               maxBytes = 1048576,
+                                               backupCount = 24,
+                                               )
+my_logger.addHandler(handler)
+
+
 class NwulabSpiderSpider(scrapy.Spider):
     name = "nwulab"
     allowed_domains = ["ipc.nwu.edu.cn"]
@@ -13,4 +29,5 @@ class NwulabSpiderSpider(scrapy.Spider):
             item = NWUItem()
             item['title'] = sel.xpath('strong/a/text()').extract()
             item['link'] = sel.xpath('strong/a/@href').extract()
+            my_logger.info(item)
             yield item
