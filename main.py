cae037eb4ed68d4a1fc92d2a3f4f21b42f0fbafd
ianhhhhhhhhe <517382432@qq.com>
2017-02-12 23:07:15 +0800

    Twisted supported

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from twisted.internet import reactor
+from twisted.internet import reactor, defer
 
 from scrapy.crawler import CrawlerRunner
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from scrapy.utils.log import configure_logging
+
+import logging
 
 from nwu.spiders.nwu_spider import NWUSpider
 from nwu.spiders.nwulab_spider import NWUlabSpider
 
-process = CrawlerProcess(get_project_settings())
+configure_logging(get_project_settings())
+runner = CrawlerRunner(get_project_settings())
 
-process.crawl(NWUSpider)
-process.crawl(NWUlabSpider)
+@defer.inlineCallbacks
+def crawl():
+    yield runner.crawl(NWUSpider)
+    yield runner.crawl(NWUlabSpider)
+    reactor.stop()
 
-process.start()
+crawl()
+reactor.run()
